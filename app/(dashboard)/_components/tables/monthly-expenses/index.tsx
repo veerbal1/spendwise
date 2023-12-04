fc32c6717c80c2f88bc7127af8136028b32b423c
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-04 13:56:37 +0530

    Updated monthly expenses table

--- app/(dashboard)/_components/tables/monthly-expenses/index.tsx
+++ app/(dashboard)/_components/tables/monthly-expenses/index.tsx
@@ -10,25 +10,30 @@ import { getUserExpenses } from '@/lib/db-queries';
 import { formatCurrencyInINR } from '@/lib/format';
 
 async function MonthlyExpensesTable() {
-  const date = new Date();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const { rows } = await getUserExpenses(month, year);
+  const today = new Date();
+  const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+
+  const { rows } = await getUserExpenses(
+    firstDayOfMonth.toDateString(),
+    today.toDateString()
+  );
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead>Name</TableHead>
-          <TableHead>Category</TableHead>
           <TableHead>Amount</TableHead>
+          <TableHead>Category</TableHead>
+          <TableHead>Date</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {rows?.map((row) => (
           <TableRow key={row.id}>
             <TableCell>{row.description}</TableCell>
-            <TableCell>{row.category_name}</TableCell>
             <TableCell>{formatCurrencyInINR(row.amount)}</TableCell>
+            <TableCell>{row.category_name}</TableCell>
+            <TableCell>{row.date.toDateString()}</TableCell>
           </TableRow>
         ))}
       </TableBody>

862a0b81abd8eaad8b918d2cec5b833280326980
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-04 17:24:20 +0530

    Updated sidebar padding

--- app/_components/sidebar/index.tsx
+++ app/_components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 function Sidebar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-60 p-4">
+    <div className="w-60 px-4">
       <div className="flex gap-1 flex-col">{children}</div>
     </div>
   );

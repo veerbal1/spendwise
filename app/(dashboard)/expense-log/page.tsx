9d545bf4bd0a6ffa404a44c8d0bc1591ea35baa1
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-04 17:24:09 +0530

    Added expense table in expense log

--- app/(dashboard)/expense-log/page.tsx
+++ app/(dashboard)/expense-log/page.tsx
@@ -1,5 +1,22 @@
+import { Suspense } from 'react';
+import MonthlyExpensesTable from '../_components/tables/monthly-expenses';
+import TodaysExpensesTable from '../_components/tables/todays-expenses';
+
 function ExpenseLog() {
-  return <div>Expense Log</div>;
+  return (
+    <div className="flex flex-col md:flex-row gap-4">
+      <div className="flex-1 border rounded-lg">
+        <Suspense fallback={"Getting today's expenses..."}>
+          <TodaysExpensesTable />
+        </Suspense>
+      </div>
+      <div className="flex-1 border rounded-lg">
+        <Suspense fallback={'Getting monthly expenses...'}>
+          <MonthlyExpensesTable />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseLog;
